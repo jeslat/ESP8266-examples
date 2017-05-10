35e396c597bdc77cf48d0fb021cc0f8ca63e34bd
Jesús Latorre <jesuslatorreasensio@gmail.com>
2017-05-10 09:02:14 +0200

    Add connect to WiFi example

---
+++ WiFi/src/main.cpp
@@ -0,0 +1,27 @@
+#include <WiFiClient.h>
+#include <ESP8266mDNS.h>
+
+//TODO: Add the name and the password of the WiFi
+char ssid[] = "";
+char password[] = "";
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Wifi test!");
+
+  //Connect to the WiFi using the name and the password
+  WiFi.begin(ssid, password);
+  //Wait until the connection is ready
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  //Print the local IP of the board
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+}
