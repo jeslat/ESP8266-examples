7d8b28dd3a150f394ed9a48d8d36eccaae4d6e89
Jesús Latorre <jesuslatorreasensio@gmail.com>
2017-05-10 09:02:26 +0200

    Add web server example

---
+++ Server/src/main.cpp
@@ -0,0 +1,51 @@
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+//TODO: Add the name and the password of the WiFi
+char ssid[] = "";
+char password[] = "";
+
+ESP8266WebServer server(80);
+
+//Method to handle the http get request
+void handleRequest() {
+  Serial.println("Request received");
+  String body = "<h1>Hello world!</h1>";
+  //Send the response with the body
+  server.send(200, "text/html", body);
+}
+
+//Method to handle the not found case
+void handleNotFound() {
+  Serial.println("Not found");
+  String body = "<h1>Not found :(</h1>";
+  server.send(404, "text/html", body);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Server test");
+
+  //Connect to the WiFi using the name and the password
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  //Config the web server
+  server.on(("/"), handleRequest);
+  server.onNotFound(handleNotFound);
+  server.begin();
+
+  Serial.println("Server started");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  server.handleClient();
+}
